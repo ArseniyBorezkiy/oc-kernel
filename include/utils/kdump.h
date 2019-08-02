c5a9e00fdd5b55bcf84c5e2e26bc44619ef999fc
Arsenii <Arsenii_Boretskii@epam.com>
2019-08-02 23:51:30 +0500

    Elf dump

--- include/utils/kdump.h
+++ include/utils/kdump.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <lib/stdtypes.h>
+#include <boot/multiboot.h>
+#include <vfs/elf.h>
 
 /*
  * Api
  */
 extern void kdump_stack(size_t *addr);
 extern void kdump_multiboot(struct mutliboot_t *header);
+extern void kdump_elf(struct elf_header_t *header);
--- kernel/utils/kdump.c
+++ kernel/utils/kdump.c
@@ -1,4 +1,5 @@
 #include <boot/multiboot.h>
+#include <vfs/elf.h>
 #include <utils/kdump.h>
 #include <utils/kprint.h>
 #include <lib/stdtypes.h>
@@ -22,4 +23,25 @@ extern void kdump_multiboot(struct multiboot_t *header) {
   kprint("-- multiboot header dump: %X\n", (size_t)header);
   kprint("  mem_lower : %X Kb\n", header->mem_lower);
   kprint("  mem_upper : %X Kb\n", header->mem_upper);
+}
+
+/*
+ * Api - Elf header dump
+ */
+extern void kdump_elf(struct elf_header_t *header) {
+  kprint("-- elf header dump: %X\n", (size_t)header);
+  kprint("  entry point: %X\n", header->e_entry);
+  kprint("  headers count: %X\n", header->e_phnum);
+  kprint("  headers begin: %X\n", header->e_shoff);
+
+  for (int i = 0; i < header->e_phnum; ++i) {
+    struct elf_program_header_t *p_header = header->e_shoff + i * header->e_ehsize;
+    kprint("  + program header dump: %X\n", p_header);
+    kprint("    segment type: %X\n", p_header->p_type);
+    kprint("    target virtual address: %X\n", p_header->p_vaddr);
+    kprint("    segment size in memory: %X\n", p_header->p_memsz);
+    kprint("    segment offset from file begin: %X\n", p_header->p_offset);
+    kprint("    segment size in file: %X\n", p_header->p_filesz);
+    kprint("    alignment: %X\n", p_header->p_align);
+  }
 }
