f89735cd83cf57700e4079770ddad5be18f5c051
Arsenii_Boretskii <arseniiboretskii@gmail.com>
2019-08-29 17:37:16 +0300

    Added fs header

--- include/vfs/initrd.h
+++ include/vfs/initrd.h
@@ -3,6 +3,21 @@
 #include <lib/stdtypes.h>
 #include <boot/multiboot.h>
 
+#define INITRD_MAX_FILES 8
+
+extern struct initrd_node_t
+{
+  char name[8]; /* file name */
+  size_t base;  /* file begin */
+  size_t size;  /* file size */
+};
+
+extern struct initrd_fs_t
+{
+  struct initrd_node_t *first[INITRD_MAX_FILES]; /* first node */
+  int count;                                     /* nodes count */
+};
+
 /*
  * Api
  */
